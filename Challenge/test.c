97e03fb6cf81abb000c4da13e061be9729ab8fcf
sudhanshu <sudhanshu.github@gmail.com>
2019-07-10 16:53:44 +0530

    First Commit

---
+++ Challenge/test.c
@@ -0,0 +1,16 @@
+/*
+Author: Sudhanshu Kumar Singh
+Purpose: This program prints out my first name on to the screen
+Date: 09/07/2019
+
+*/
+
+
+#include<stdio.h>
+
+int main(void)
+{
+    // This code is displaying my first name
+    printf("Hi, My first name is Sudhanshu Kumar\n");
+    return 0;
+}
